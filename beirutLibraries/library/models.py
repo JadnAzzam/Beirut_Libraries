f849d85416947600a4026476dd3a21d7d2f69702
elie-01 <elie.mina@outlook.com>
2021-11-30 00:46:44 +0200

    complete models

--- beirutLibraries/library/models.py
+++ beirutLibraries/library/models.py
@@ -9,16 +9,25 @@ class Book(models.Model):
     rating = models.IntegerField()
     description = models.CharField(max_length=200)
     id = models.IntegerField(primary_key=True)
-    # add min max value
+    datePublished = models.DateTimeField(auto_now_add = True)
+    
 
     def __str__(self):
         return self.title
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    client = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     id = models.IntegerField(primary_key=True)
     email = models.CharField(max_length = 100)
     phoneNumber = models.CharField(max_length=8)
-    # signUpDate = models.DateTimeField(auto_now_add = True)
+    signUpDate = models.DateTimeField(auto_now_add = True)
+
+
+class Librarian(models.Model):
+    librarian = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    id = models.IntegerField(primary_key=True)
+    email = models.CharField(max_length = 100)
+    phoneNumber = models.CharField(max_length=8)
+    signUpDate = models.DateTimeField(auto_now_add = True)
 
