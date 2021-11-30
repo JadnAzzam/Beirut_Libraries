ab72a0b301fbf0957b623ca1ea76e1dbb94a1aca
elie-01 <elie.mina@outlook.com>
2021-11-30 05:11:37 +0200

    First views

--- beirutLibraries/library/urls.py
+++ beirutLibraries/library/urls.py
@@ -5,8 +5,20 @@ from . import views
 from django.conf import settings
 # from django.conf.urls.static import static
 
+app_name = 'library'
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'home_page')),
-    path('login/', views.login_view, name = "user-login")
+    path('', TemplateView.as_view(template_name = 'home_page'), name = 'home'),
+    path('login-user/', views.login_user_view, name = "login-user"),
+    path('logout-user/', views.logout_user_views, name = "logout-user"),
+    path('register-user/', views.register_user_views, name = "register-user"),
+    path('login-admin/', views.librarian_login, name = "login-librarian"),
+    path('logout-admin/', views.logout_librarian_views, name = "logout-librarian"),
+    path('register-admin/', views.register_librarian_views, name = "register-librarian"),
+    path('list/', views.book_list_view, name = 'book_list'),
+    path('create/', views.book_create_view, name='create-book'),
+    path('<int:book_id>/', views.book_detail_view, name="book-detail"),
+    path('<int:book_id>/delete/', views.book_delete_view, name='book-delete'),
+    path('<int:book_id>/update/', views.book_update_view, name='book-update'),
 ] 
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- beirutLibraries/library/views.py
+++ beirutLibraries/library/views.py
@@ -2,10 +2,35 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from .forms import RegisterUserForm, RegisterLibrarianForm, UserForm, LibrarianForm, CreateBookForm, BookForm
 from django.contrib import messages
-from django.contrib.auth import login
-from django.contrib.auth import logout
-from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-##from .decorators import unauthenticated_user, allowed_users
+from .decorators import unauthenticated_user, allowed_users
 from django.contrib.auth.models import Group
 from .models import Book, Student, Librarian
+
+@unauthenticated_user
+def login_user_view(request):
+  if request.method == "POST":
+    form = AuthenticationForm(request = request, data = request.POST)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.info(request,f"You are now logged in as {username}")
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid Username or Password.')
+    else:
+        messages.error(request, "Invalid username or password.")
+  form = AuthenticationForm()
+  #returns the login page
+  context = {'form':form}
+  return render(request,'main/login.html',context)
+
+
+def logoutUser(request):
+  logout(request)
+  list(messages.get_messages(request))
+  return redirect('login')
